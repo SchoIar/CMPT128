fb1678a115cc09f49005f4ccaa19aeeb64449d13
Anton <93786227+SchoIar@users.noreply.github.com>
2021-11-24 14:00:03 -0800

    Create whatdoesthisdo.c

---
+++ whatdoesthisdo/whatdoesthisdo.c
@@ -0,0 +1,19 @@
+/*
+#include <stdio.h>
+int main(void)
+{
+      int x = 10;
+      int y = 10;
+      int myprefix = ++x;
+      int mypostfix = y++;
+      printf("The prefix result: %d, the postfix result: %d\n", myprefix,
+      mypostfix);
+      
+      int arr[3] = {1, 2, 3};
+      size_t arrsize = sizeof(arr);
+      printf("Total array size in bytes: %ld\n", arrsize);
+//sizeif prints the size of the array
+
+}//PRINTS "11" then "10"
+*/
+
