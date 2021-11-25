1df7e36d41737e08a997bbaef05fa730951a9f21
Anton <93786227+SchoIar@users.noreply.github.com>
2021-11-24 16:36:58 -0800

    Update whatdoesthisdo.c

--- whatdoesthisdo/whatdoesthisdo.c
+++ whatdoesthisdo/whatdoesthisdo.c
@@ -1,4 +1,5 @@
 /*
+                                    //.... sizeof(int);
 #include <stdio.h>
 int main(void)
 {
@@ -17,3 +18,32 @@ int main(void)
 }//PRINTS "11" then "10"
 */
 
+/*
+                                    //DECLAIRING A POINTER: some_type* pointer_name;
+
+#include <stdio.h>
+int main(void)
+{
+      int arr[5] = {10, 20, 30, 40, 50};
+      int *p = arr; /* p now points at the first array element */
+      printf("The first array element is: %d\n", *p);
+}
+*/
+      
+/*
+                                                      //functions
+#include <stdio.h>
+int mySum(int x, int y)
+{
+return x + y; }
+int main(void)
+{
+      int myresult = mySum(10, 20);
+      printf("The result is: %d\n", myresult);
+}
+
+*/
+  
+      
+      
+      
